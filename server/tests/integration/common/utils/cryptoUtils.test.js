d08196b8f46f975ae15dee2828843330d1c708d2
Samir <sbenfares@gmail.com>
2022-08-29 10:34:26 +0200

    feat(server): add crypto utils

---
+++ server/tests/integration/common/utils/cryptoUtils.test.js
@@ -0,0 +1,19 @@
+import { strict as assert } from "assert";
+import { generateRandomAlphanumericPhrase } from "../../../../src/common/utils/cryptoUtils.js";
+
+describe("generateRandomAlphanumericPhrase", () => {
+  it("crée une chaîne de caractère aléatoire de longueur demandée", () => {
+    const randomPhrase = generateRandomAlphanumericPhrase(13);
+    assert.equal(randomPhrase.length, 13);
+  });
+
+  it("crée une chaîne de caractère aléatoire de longueur 20 lorsqu'aucun longueur n'est passée", () => {
+    const randomPhrase = generateRandomAlphanumericPhrase();
+    assert.equal(randomPhrase.length, 20);
+  });
+
+  it("crée une chaîne de caractère aléatoire ne contenant que des caractères alphanumériques", () => {
+    const randomPhrase = generateRandomAlphanumericPhrase();
+    assert.equal(/^[a-zA-Z0-9]*$/.test(randomPhrase), true);
+  });
+});
