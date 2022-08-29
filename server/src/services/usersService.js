5a267e9a52bf77ec0d7bcef7f51a2cf0ddabef2e
Samir <sbenfares@gmail.com>
2022-08-29 12:11:51 +0200

    feat(server): users generatePasswordUpdateToken & updatePassword

--- server/src/services/usersService.js
+++ server/src/services/usersService.js
@@ -3,6 +3,10 @@ import { generateRandomAlphanumericPhrase, hash } from "../common/utils/cryptoUt
 import { UsersFactory } from "../factory/usersFactory.js";
 import { COLLECTIONS_NAMES } from "../model/collections/index.js";
 import { dbCollection } from "../model/db/mongodbClient.js";
+import { addHours, isBefore } from "date-fns";
+import { validatePassword } from "../domain/password.js";
+
+const PASSWORD_UPDATE_TOKEN_VALIDITY_HOURS = 48;
 
 /**
  * Méthode de création d'un utilisateur
@@ -49,4 +53,80 @@ const create = async (userProps) => {
   return insertedId;
 };
 
-export default () => ({ create });
+/**
+ * Méthode de génération d'un token pour update du mot de passe de l'utilisateur
+ * @param {*} username
+ * @returns
+ */
+const generatePasswordUpdateToken = async (username) => {
+  const user = await dbCollection(COLLECTIONS_NAMES.Users).findOne({ username });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const token = generateRandomAlphanumericPhrase(80); // 1 hundred quadragintillion years to crack https://www.security.org/how-secure-is-my-password/
+
+  // MAJ l'utilisateur avec un token et un token_expiry
+  await dbCollection(COLLECTIONS_NAMES.Users).findOneAndUpdate(
+    { username: username },
+    {
+      $set: {
+        password_update_token: token,
+        password_update_token_expiry: addHours(new Date(), PASSWORD_UPDATE_TOKEN_VALIDITY_HOURS),
+      },
+    }
+  );
+
+  return token;
+};
+
+/**
+ * Vérifie que le token du user est valide et l'expiry valide
+ * @param {*} user
+ * @returns
+ */
+const isUserPasswordUpdatedTokenValid = (user) => {
+  return Boolean(user.password_update_token_expiry) && isBefore(new Date(), user.password_update_token_expiry);
+};
+
+/**
+ * Méthode de maj du password du user
+ * @param {*} updateToken
+ * @param {*} password
+ * @returns
+ */
+const updatePassword = async (updateToken, password) => {
+  if (!validatePassword(password)) throw new Error("Password must be valid (at least 16 characters)");
+
+  // find user with password_update_token and ensures it exists
+  const user = await dbCollection(COLLECTIONS_NAMES.Users).findOne({
+    password_update_token: updateToken,
+    password_update_token_expiry: { $ne: null },
+  });
+
+  // throw if user is not found
+  if (!user) throw new Error("User not found");
+
+  // token must be valid
+  if (!isUserPasswordUpdatedTokenValid(user)) {
+    throw new Error("Password update token has expired");
+  }
+
+  // MAJ l'utilisateur avec un password et reset du token & token policy
+  const updated = await dbCollection(COLLECTIONS_NAMES.Users).findOneAndUpdate(
+    { _id: user._id },
+    {
+      $set: {
+        password: hash(password),
+        password_update_token: null,
+        password_update_token_expiry: null,
+      },
+    },
+    { new: true }
+  );
+
+  return updated;
+};
+
+export default () => ({ create, generatePasswordUpdateToken, updatePassword });
