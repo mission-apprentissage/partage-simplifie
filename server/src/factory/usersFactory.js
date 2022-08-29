d7adfc03e648e0294915ab6618e915dc47af4089
Samir <sbenfares@gmail.com>
2022-08-29 10:46:22 +0200

    feat(server): add usersFactory & unit tests

---
+++ server/src/factory/usersFactory.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+import { ROLES } from "../common/constants/roles.js";
+import { BaseFactory } from "./baseFactory.js";
+
+export class UsersFactory extends BaseFactory {
+  /**
+   * Crée une entité User à partir de props
+   * @param {*} props
+   * @returns
+   */
+  static create(props) {
+    const schema = Joi.object({
+      username: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      role: Joi.string()
+        .valid(...Object.values(ROLES))
+        .required(),
+      nom: Joi.string().allow("", null),
+      prenom: Joi.string().allow("", null),
+      fonction: Joi.string().allow("", null),
+      telephone: Joi.string().allow("", null),
+      outils_gestion: Joi.array().items(Joi.string()).allow(null),
+      nom_etablissement: Joi.string().allow("", null),
+    });
+
+    const { error } = schema.validate(props);
+    if (error) return null;
+
+    return {
+      ...props,
+      created_at: new Date(),
+      updated_at: null,
+    };
+  }
+}
+
+export default { UsersFactory };
