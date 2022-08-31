86946ea4f7cdb678f70cd3a8b2ee6f179ff8ab6e
Samir <sbenfares@gmail.com>
2022-08-31 10:08:27 +0200

    feat(server): add missing indexer for users

--- server/src/model/indexes/index.js
+++ server/src/model/indexes/index.js
@@ -4,9 +4,11 @@ import { BaseIndexer } from "./baseIndexer.js";
 export const createIndexes = async () => {
   await new BaseIndexer(COLLECTIONS_NAMES.UserEvents, COLLECTIONS_INDEXS.UserEvents).createIndexs();
   await new BaseIndexer(COLLECTIONS_NAMES.JobEvents, COLLECTIONS_INDEXS.JobEvents).createIndexs();
+  await new BaseIndexer(COLLECTIONS_NAMES.Users, COLLECTIONS_INDEXS.Users).createIndexs();
 };
 
 export const dropIndexes = async () => {
   await new BaseIndexer(COLLECTIONS_NAMES.UserEvents, COLLECTIONS_INDEXS.UserEvents).dropIndexs();
   await new BaseIndexer(COLLECTIONS_NAMES.JobEvents, COLLECTIONS_INDEXS.JobEvents).dropIndexs();
+  await new BaseIndexer(COLLECTIONS_NAMES.Users, COLLECTIONS_INDEXS.Users).dropIndexs();
 };
