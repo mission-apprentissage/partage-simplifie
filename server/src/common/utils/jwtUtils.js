dda5d2c9d6d42d355cac3839e1c1d7efb192389a
Samir <sbenfares@gmail.com>
2022-09-02 17:29:16 +0200

    feat(server): simplify jwt

--- server/src/common/utils/jwtUtils.js
+++ server/src/common/utils/jwtUtils.js
@@ -2,33 +2,20 @@ import jwt from "jsonwebtoken";
 import { config } from "../../../config/index.js";
 
 /**
- * Création d'un token générique
- * @param {*} type
- * @param {*} subject
- * @param {*} options
- * @returns
- */
-export const createToken = (type, subject, options = {}) => {
-  const defaults = config.auth[type];
-  const secret = options.secret || defaults.jwtSecret;
-  const expiresIn = options.expiresIn || defaults.expiresIn;
-  const payload = options.payload || {};
-
-  return jwt.sign(payload, secret, {
-    issuer: config.appName,
-    expiresIn: expiresIn,
-    subject: subject,
-  });
-};
-
-/**
  * Création d'un token utilisateur
  * @param {*} user
- * @param {*} options
  * @returns
  */
-export const createUserToken = (user, options = {}) => {
+export const createUserToken = (user) => {
   // Liste des champs propres à l'utilisateur à ajouter au payload du token
   const payload = { role: user.role, nom_etablissement: user.nom_etablissement || null };
-  return createToken("user", user.email, { payload, ...options });
+  const subject = user.email;
+  const jwtSecret = config.auth.user.jwtSecret;
+  const expiresIn = config.auth.user.expiresIn;
+
+  return jwt.sign(payload, jwtSecret, {
+    issuer: config.appName,
+    expiresIn,
+    subject,
+  });
 };
