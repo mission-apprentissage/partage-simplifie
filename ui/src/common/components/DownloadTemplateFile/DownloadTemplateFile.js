9cc08b57ecad1b4f2cf86e65ca53e00607fbb108
Samir <sbenfares@gmail.com>
2022-09-08 15:14:35 +0200

    fix(ui): Download block width

--- ui/src/common/components/DownloadTemplateFile/DownloadTemplateFile.js
+++ ui/src/common/components/DownloadTemplateFile/DownloadTemplateFile.js
@@ -4,23 +4,25 @@ import { TEMPLATE_FILE } from "../../constants/product.js";
 
 const DownloadTemplateFile = () => {
   return (
-    <a target="_blank" rel="noopener noreferrer" href={TEMPLATE_FILE.PATH}>
-      <Box width="40%" border="1px solid" borderColor="#DDDDDD" padding="4w" marginTop="6w">
-        <Heading fontSize="22px" as="h2">
-          Télécharger le document de dépôt de données
-        </Heading>
-        <Text fontSize="zeta" color="gray.800" marginTop="2w">
-          Une fois téléchargé, remplissez-le en suivant les champs demandés, puis revenez sur votre compte pour le
-          téléverser.
-        </Text>
-        <Flex marginTop="4w">
-          <Text marginTop="1w" fontSize="omega" color="#666666" textDecoration="underline" flex="1">
-            {TEMPLATE_FILE.EXTENSION} – {TEMPLATE_FILE.SIZE} Ko
+    <Box width="40%">
+      <a target="_blank" rel="noopener noreferrer" href={TEMPLATE_FILE.PATH}>
+        <Box border="1px solid" borderColor="#DDDDDD" padding="4w" marginTop="6w">
+          <Heading fontSize="22px" as="h2">
+            Télécharger le document de dépôt de données
+          </Heading>
+          <Text fontSize="zeta" color="gray.800" marginTop="2w">
+            Une fois téléchargé, remplissez-le en suivant les champs demandés, puis revenez sur votre compte pour le
+            téléverser.
           </Text>
-          <Box as="i" color="bluefrance" fontSize="beta" className="ri-download-line" />
-        </Flex>
-      </Box>
-    </a>
+          <Flex marginTop="4w">
+            <Text marginTop="1w" fontSize="omega" color="#666666" textDecoration="underline" flex="1">
+              {TEMPLATE_FILE.EXTENSION} – {TEMPLATE_FILE.SIZE} Ko
+            </Text>
+            <Box as="i" color="bluefrance" fontSize="beta" className="ri-download-line" />
+          </Flex>
+        </Box>
+      </a>
+    </Box>
   );
 };
 
