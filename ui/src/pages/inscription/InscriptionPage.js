76bb4df0dbc4682bd4ce0365247a69a1a5fb72a0
Samir <sbenfares@gmail.com>
2022-09-15 17:16:27 +0200

    feat(ui): pass organisme to sessionStorage from confirmOf Modal to register

--- ui/src/pages/inscription/InscriptionPage.js
+++ ui/src/pages/inscription/InscriptionPage.js
@@ -1,10 +1,16 @@
 import { Box, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 import { Page, Section } from "../../common/components";
+import { NAVIGATION_PAGES } from "../../common/constants/navigationPages.js";
+import { SESSION_STORAGE_ORGANISME } from "../../common/constants/sessionStorageConstants.js";
 import { FranceLocalization } from "../../theme/components/icons/FranceLocalization.js";
 
 const InscriptionPage = () => {
+  if (!sessionStorage.getItem(SESSION_STORAGE_ORGANISME)) return <Redirect to={NAVIGATION_PAGES.Accueil.path} />;
+  const { uai, siret, siren, nature } = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_ORGANISME)) || null;
+
   return (
     <Page>
       <Section withShadow paddingY="4w" color="grey.800">
@@ -15,6 +21,10 @@ const InscriptionPage = () => {
                 Veuillez compléter les informations suivantes pour créer votre compte :
               </Heading>
               <Text marginTop="3w">Page en cours de développement</Text>
+              <Text>{uai}</Text>
+              <Text>{siret}</Text>
+              <Text>{siren}</Text>
+              <Text>{nature}</Text>
             </Stack>
           </Box>
           <Box>
