fea722f7ac381d710e973e2338d3e2db4204f2c0
Samir B <sbenfares@gmail.com>
2022-09-22 21:22:57 +0200

    feat(server): add getUploadHistoryList to userEventsService + tests

--- server/src/services/userEventsService.js
+++ server/src/services/userEventsService.js
@@ -1,3 +1,4 @@
+import { USER_EVENTS_ACTIONS } from "../common/constants/userEventsConstants.js";
 import { UserEventsFactory } from "../factory/userEventsFactory.js";
 import { COLLECTIONS_NAMES } from "../model/collections/index.js";
 import { dbCollection } from "../model/db/mongodbClient.js";
@@ -13,4 +14,26 @@ const createUserEvent = async ({ user_email, type, action, data }) => {
   return;
 };
 
-export default () => ({ createUserEvent });
+/**
+ * Récupération de l'historique des téléversements pour un mail utilisateur
+ * @param {*} param0
+ * @returns
+ */
+const getUploadHistoryList = async ({ user_email }) => {
+  const userEventsUploadSuccessForUserMail = await dbCollection(COLLECTIONS_NAMES.UserEvents)
+    .find({
+      user_email: user_email,
+      action: USER_EVENTS_ACTIONS.UPLOAD.SUCCESS,
+      "data.originalname": { $exists: true },
+    })
+    .project({ "data.originalname": 1, created_at: 1 })
+    .sort({ created_at: -1 })
+    .toArray();
+
+  return userEventsUploadSuccessForUserMail.map((item) => ({
+    nom_fichier: item.data.originalname,
+    date_creation: item.created_at,
+  }));
+};
+
+export default () => ({ createUserEvent, getUploadHistoryList });
