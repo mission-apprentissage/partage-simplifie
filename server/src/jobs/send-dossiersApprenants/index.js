19cbcb2b98dd6d33ee875f4a078e575aa64bdf19
Samir B <sbenfares@gmail.com>
2022-10-07 18:08:23 +0200

    feat(server): sendToTdb job

--- server/src/jobs/send-dossiersApprenants/index.js
+++ server/src/jobs/send-dossiersApprenants/index.js
@@ -6,19 +6,96 @@ import {
   testAuthDossierApprenants,
 } from "../../common/apis/apiTdbMna.js";
 import { logger } from "../../common/logger/logger.js";
+import { COLLECTIONS_NAMES } from "../../model/collections/index.js";
+import { dbCollection } from "../../model/db/mongodbClient.js";
+import { asyncForEach } from "../../common/utils/asyncUtils.js";
+import { toDossiersApprenantsList } from "../../model/api/donneesApprenantsMapper.js";
+import { JOB_NAMES } from "../../common/constants/jobsConstants.js";
+import { splitIntoChunksList } from "../../common/utils/arrayUtils.js";
 
-export const runSendDossiersApprenantsToTdb = async () => {
+const TDB_DOSSIERSAPPRENANT_CHUNK_SIZE = 99;
+
+export const runSendDossiersApprenantsToTdb = async (jobEvents) => {
   logger.info("Envoi des données apprenants sous forme de dossiersApprenants à l'API TDB");
 
   // Check API & Log User
   await checkApiAvailable();
   const bearerToken = await logApiUser();
 
-  // TODO ForEachDonneesByUser
-  // TODO Transform
-  // TODO Split by 100 & Post + UserEvent
-  const postResponse = await postDossierApprenants({ bearerToken, dossiersApprenants: [] });
-  logger.info(postResponse);
+  // Traitement de la liste des donnees pour chaque utilisateur
+  const distinctUsersEmail = await dbCollection(COLLECTIONS_NAMES.DonneesApprenants).distinct("user_email");
+  logger.info(`${distinctUsersEmail.length} utilisateurs différents ayant des donnéesApprenants à envoyer au TDB`);
+
+  // Pour chaque utilisateur identifié on transforme ses données puis on les envoie au TDB
+  await asyncForEach(distinctUsersEmail, async (currentUserEmail) => {
+    logger.info(`...Traitement des données pour l'utilisateur ${currentUserEmail} `);
+
+    const donneesApprenantsForUser = await dbCollection(COLLECTIONS_NAMES.DonneesApprenants)
+      .find({ user_email: currentUserEmail })
+      .toArray();
+
+    // Construction d'une liste de dossiersApprenants en mappant chaque élément
+    const dossiersApprenantsForUser = [];
+    donneesApprenantsForUser.forEach((currentDonneeApprenant) => {
+      dossiersApprenantsForUser.push(...toDossiersApprenantsList(currentDonneeApprenant));
+    });
+
+    // Envoi à l'api de la liste + log userEvent
+    await sendDossiersApprenantsForUserToTdb(dossiersApprenantsForUser, bearerToken, currentUserEmail, jobEvents);
+
+    logger.info(`Envoi des données pour l'utilisateur ${currentUserEmail} terminé !`);
+  });
+};
+
+/**
+ * Méthode d'envoi des dossiersApprenants à l'API du TDB
+ * @param {*} dossiersApprenantsToSend
+ * @param {*} bearerToken
+ */
+const sendDossiersApprenantsForUserToTdb = async (
+  dossiersApprenantsToSend,
+  bearerToken,
+  currentUserEmail,
+  jobEvents
+) => {
+  const dossiersApprenantsChunksList = splitIntoChunksList(dossiersApprenantsToSend, TDB_DOSSIERSAPPRENANT_CHUNK_SIZE);
+
+  for (const key in dossiersApprenantsChunksList) {
+    const chunkToSend = dossiersApprenantsChunksList[key];
+
+    try {
+      const { ok, ko, status, message } = await postDossierApprenants({
+        bearerToken,
+        dossiersApprenants: chunkToSend,
+      });
+
+      if (status === "KO") {
+        logger.error(`Envoi des dossiersApprenants en erreur : ${message}`);
+      } else {
+        logger.info(`-> ${ok} dossiersApprenants valides lors de l'envoi du chunk...`);
+        logger.info(`-> ${ko} dossiersApprenants non valides lors de l'envoi du chunk...`);
+      }
+
+      // Log des dossiers envoyées
+      await logDossiersApprenantsSentForUser(jobEvents, chunkToSend, currentUserEmail);
+    } catch (err) {
+      logger.err(`Erreur lors de l'envoi des données pour l'utilisateur ${currentUserEmail}`);
+      logger.err(`Erreur ${err}`);
+    }
+  }
+};
+
+/**
+ * Log des dossiersApprenants envoyés
+ * @param {*} jobEvents
+ * @param {*} dossiersApprenantsToLog
+ */
+const logDossiersApprenantsSentForUser = async (jobEvents, dossiersApprenantsToLog, userEmail) => {
+  await jobEvents.createJobEvent({
+    jobname: JOB_NAMES.sendDossiersApprenants,
+    action: "dossiersApprenants-sent",
+    data: { user_email: userEmail, dossiersApprenants: dossiersApprenantsToLog },
+  });
 };
 
 /**
