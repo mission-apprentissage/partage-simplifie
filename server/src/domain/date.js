6db8963b4990d8f4ed3c1356038a6f1a12ea416a
Samir B <sbenfares@gmail.com>
2022-10-12 15:50:26 +0200

    chore: change date format to jj/MM/aaaa (#143)

--- server/src/domain/date.js
+++ server/src/domain/date.js
@@ -1,3 +1,3 @@
 import { parse } from "date-fns";
 
-export const parseFormattedDate = (value) => (value ? parse(value, "dd-MM-yyyy", new Date()) : null);
+export const parseFormattedDate = (value) => (value ? parse(value, "dd/MM/yyyy", new Date()) : null);
